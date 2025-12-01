6337714a02690bf85a7dccd8e3fb83574587dbee
Ilaya <linoyabutbul@gmail.com>
2025-12-01 12:14:05 +0200

    initial commit

--- src/main/java/com/example/biztrack/repository/AppointmentRepository.java
+++ src/main/java/com/example/biztrack/repository/AppointmentRepository.java
@@ -1,5 +1,4 @@
 package com.example.biztrack.repository;
-
 import com.example.biztrack.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
